81f4d3e82c5f4878f4ebadc7e0cde1b68b28e24f
zackz9 <zackel7593@gmail.com>
2023-01-14 00:24:20 +0100

    Enjoy searching movies

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import MovieCard from './MovieCard'
 import './App.css'
 import SearchIcon from './search.svg'
 
-const API_URL = 'http://www.omdbapi.com/?apikey=d4b0a634'
+const API_URL = 'https://www.omdbapi.com/?apikey=d4b0a634'
 
 const App = () => {
 
