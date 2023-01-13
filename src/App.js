9526aa4c3d4baf6423452c0fe9d1766825251e71
zackz9 <zackel7593@gmail.com>
2023-01-14 00:20:53 +0100

    Enjoy searching movies

--- src/App.js
+++ src/App.js
@@ -4,13 +4,6 @@ import './App.css'
 import SearchIcon from './search.svg'
 
 const API_URL = 'http://www.omdbapi.com/?apikey=d4b0a634'
-const movie1 = {
-    "Title": "Who Killed the Electric Car?",
-    "Year": "2006",
-    "imdbID": "tt0489037",
-    "Type": "movie",
-    "Poster": "https://m.media-amazon.com/images/M/MV5BMTc1MTM0MTA2N15BMl5BanBnXkFtZTcwMTk1OTIzMQ@@._V1_SX300.jpg"
-}
 
 const App = () => {
 
